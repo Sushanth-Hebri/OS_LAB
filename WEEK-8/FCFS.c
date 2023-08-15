320e9e1b8b97e0f8cb745acd1701bd3a51df2d92
Sushanth <117469085+Sushanth-Hebri@users.noreply.github.com>
2023-08-16 00:25:32 +0530

    Add files via upload

---
+++ WEEK-8/FCFS.c
@@ -0,0 +1,28 @@
+
+// Write a C program to simulate disk scheduling algorithms
+// a)  FCFS
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n,head;
+printf("First come first serve\n");
+printf("Enter the number of request(n):");
+scanf("%d",&n);
+int i,rs[n];
+printf("Enter request sequence one by one\n");
+for( i=0;i<n;i++){
+scanf("%d",&rs[i]);
+}
+printf("Enter initial head position(p):");
+scanf("%d",&head);
+int sum=0;
+sum=abs(head-rs[0]);
+
+for(int j=1;j<n;j++){
+    sum=sum+abs(rs[j]-rs[j-1]);
+}
+
+printf("Total seek operation:%d",sum);
+return 0;
+}
